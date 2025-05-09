33fdbe5395a379d9bfdb7c8fb2690818500b1ae9
Muhammad Usman Ghani <muhammadusmanghani@Muhammads-MacBook-Pro.local>
2025-05-09 19:44:46 +0500

    Added Sami class and updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include "Name.h"
 #include "Andrejs.h"
+#include "Sami.h"
+
 using namespace std;
 
 int main()
 {
-	const int array_size = 1;
+	const int array_size = 2;
 	Name *Names[array_size];
 	
 	Andrejs a;
 	a.setName("Andrejs");
 	
+	Sami s;
+	s.setName("Sami");
+
 	Names[0] = &a;
-	
-	for (int i=0; i<array_size; i++)
+	Names[1] = &s;
+
+	for (int i = 0; i < array_size; i++)
 		Names[i]->Print();
-	
+
 	return 0;
 }
